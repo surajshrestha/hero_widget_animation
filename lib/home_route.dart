0e01badf7de478e1f46b5179c6604e33469b66ee
fm-pc-lt-144 <fm-pc-lt-144@FM-PC-LT-144.local>
2024-05-16 00:32:40 +0545

    animate

---
+++ lib/home_route.dart
@@ -0,0 +1,28 @@
+import 'package:animate_widget/detail_screen_route.dart';
+import 'package:flutter/material.dart';
+
+class HomeRoute extends StatelessWidget {
+  const HomeRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return DetailScreen();
+          }));
+        },
+        child: Hero(
+          tag: 'imageHero',
+          child: Image.network(
+            'https://picsum.photos/250?image=9',
+          ),
+        ),
+      ),
+    );
+  }
+}
